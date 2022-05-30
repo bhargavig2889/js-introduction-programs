3bc2b1f827962f64591d2baa9419413a3258f148
bhargavig2889 <105350113+bhargavig2889@users.noreply.github.com>
2022-05-30 13:17:32 +0530

    Add files via upload

---
+++ area of triangle.js
@@ -0,0 +1,4 @@
+var  baseValue=prompt("enter the base of a triangle")
+var   heightValue=prompt(" enter the height of the triangle")
+ var  areaValue=  (baseValue * heightValue)/2;
+console.log(" the area of triangle is" +areaValue)
